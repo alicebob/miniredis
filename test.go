ba1c4425087c2209d30c0a88b09e26387df2fe73
Harmen <alicebob@lijzij.de>
2018-02-15 21:59:53 +0100

    remove 1.9-isms
    They were only used in tests.

--- test.go
+++ test.go
@@ -7,8 +7,6 @@ import (
 
 // assert fails the test if the condition is false.
 func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
-	tb.Helper()
-
 	if !condition {
 		tb.Errorf(msg, v...)
 	}
@@ -16,8 +14,6 @@ func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 
 // ok fails the test if an err is not nil.
 func ok(tb testing.TB, err error) {
-	tb.Helper()
-
 	if err != nil {
 		tb.Errorf("unexpected error: %s", err.Error())
 	}
@@ -25,8 +21,6 @@ func ok(tb testing.TB, err error) {
 
 // equals fails the test if exp is not equal to act.
 func equals(tb testing.TB, exp, act interface{}) {
-	tb.Helper()
-
 	if !reflect.DeepEqual(exp, act) {
 		tb.Errorf("expected: %#v got: %#v", exp, act)
 	}
@@ -34,8 +28,6 @@ func equals(tb testing.TB, exp, act interface{}) {
 
 // mustFail compares the error strings
 func mustFail(tb testing.TB, err error, want string) {
-	tb.Helper()
-
 	if err == nil {
 		tb.Errorf("expected an error, but got a nil")
 	}
