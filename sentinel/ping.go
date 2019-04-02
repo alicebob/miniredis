e918dd9ba9a1025be55aeaab680fb862755ff1ff
Jim Lambert <jim_lambert@bose.com>
2019-04-02 11:06:09 -0400

    ping command

---
+++ sentinel/ping.go
@@ -0,0 +1,20 @@
+package sentinel
+
+import "github.com/alicebob/miniredis/server"
+
+func commandsPing(s *Sentinel) {
+	s.srv.Register("PING", s.cmdPing)
+}
+
+// cmdPing
+func (s *Sentinel) cmdPing(c *server.Peer, cmd string, args []string) {
+	if len(args) != 0 {
+		c.WriteError(errWrongNumber(cmd))
+		return
+	}
+	if !s.handleAuth(c) {
+		return
+	}
+
+	c.WriteInline("PONG")
+}
