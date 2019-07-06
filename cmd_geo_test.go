66f0949badad24b8eca8456415ad7341068ad11a
Harmen <alicebob@lijzij.de>
2019-07-06 09:15:35 +0200

    switch redis client in geo test

--- cmd_geo_test.go
+++ cmd_geo_test.go
@@ -3,7 +3,7 @@ package miniredis
 import (
 	"testing"
 
-	"github.com/go-redis/redis"
+	"github.com/gomodule/redigo/redis"
 )
 
 // Test GEOADD / GEORADIUS
@@ -11,40 +11,36 @@ func TestGeo(t *testing.T) {
 	s, err := Run()
 	ok(t, err)
 	defer s.Close()
-
-	client := redis.NewClient(&redis.Options{
-		Addr: s.Addr(),
-	})
-	defer client.Close()
+	c, err := redis.Dial("tcp", s.Addr())
+	ok(t, err)
+	defer c.Close()
 
 	{
-		err = client.GeoAdd("Sicily", &redis.GeoLocation{
-			Name:      "Palermo",
-			Longitude: 13.361389,
-			Latitude:  38.115556,
-		}).Err()
+		_, err := c.Do("GEOADD", "Sicily", 13.361389, 38.115556, "Palermo")
+		ok(t, err)
+		_, err = c.Do("GEOADD", "Sicily", 15.087269, 37.502669, "Catania")
 		ok(t, err)
 
-		err = client.GeoAdd("Sicily", &redis.GeoLocation{
-			Name:      "Catania",
-			Longitude: 15.087269,
-			Latitude:  37.502669,
-		}).Err()
+		res, err := redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHDIST", "WITHCOORD"))
 		ok(t, err)
-
-		res, err := client.GeoRadius("Sicily", 15, 37, &redis.GeoRadiusQuery{WithDist: true, WithCoord: true, Radius: 200}).Result()
-		ok(t, err)
+		equals(t, 2, len(res))
 		for _, loc := range res {
-			if loc.Name != "Catania" && loc.Name != "Palermo" {
-				t.Errorf("unexpected name %q", loc.Name)
+			item := loc.([]interface{})
+			var (
+				name, _     = redis.String(item[0], nil)
+				distance, _ = redis.Float64(item[1], nil)
+				coord, _    = redis.Float64s(item[2], nil)
+			)
+			if name != "Catania" && name != "Palermo" {
+				t.Errorf("unexpected name %q", name)
 			}
-			if loc.Latitude == 0.00 || loc.Longitude == 0.00 {
-				t.Errorf("latitude/longitude shouldn't be empty")
-			}
-			if loc.Dist == 0.00 {
+			if distance == 0.00 {
 				t.Errorf("distance shouldn't be empty")
 			}
+			equals(t, 2, len(coord))
+			if coord[0] == 0.00 || coord[1] == 0.00 {
+				t.Errorf("latitude/longitude shouldn't be empty")
+			}
 		}
-		equals(t, 2, len(res))
 	}
 }
