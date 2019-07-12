5e2cf96c327f08136143860aba022c18505ac4e6
Simon Klinkert <simon.klinkert@gmail.com>
2019-07-12 09:42:29 +0200

    Add some unittests for GEORADIUS

--- cmd_geo_test.go
+++ cmd_geo_test.go
@@ -21,6 +21,7 @@ func TestGeo(t *testing.T) {
 		_, err = c.Do("GEOADD", "Sicily", 15.087269, 37.502669, "Catania")
 		ok(t, err)
 
+		// GEORADIUS + WITHDIST + WITHCOORD
 		res, err := redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHDIST", "WITHCOORD"))
 		ok(t, err)
 		equals(t, 2, len(res))
@@ -42,5 +43,102 @@ func TestGeo(t *testing.T) {
 				t.Errorf("latitude/longitude shouldn't be empty")
 			}
 		}
+
+		// GEORADIUS + WITHCOORD
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHCOORD"))
+		ok(t, err)
+		equals(t, 2, len(res))
+		for _, loc := range res {
+			item := loc.([]interface{})
+			var (
+				name, _  = redis.String(item[0], nil)
+				coord, _ = redis.Float64s(item[1], nil)
+			)
+			equals(t, 2, len(item))
+			if name != "Catania" && name != "Palermo" {
+				t.Errorf("unexpected name %q", name)
+			}
+			equals(t, 2, len(coord))
+			if coord[0] == 0.00 || coord[1] == 0.00 {
+				t.Errorf("latitude/longitude shouldn't be empty")
+			}
+		}
+
+		// GEORADIUS + WITHDIST
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHDIST"))
+		ok(t, err)
+		equals(t, 2, len(res))
+		for _, loc := range res {
+			item := loc.([]interface{})
+			var (
+				name, _     = redis.String(item[0], nil)
+				distance, _ = redis.Float64(item[1], nil)
+			)
+			equals(t, 2, len(item))
+			if name != "Catania" && name != "Palermo" {
+				t.Errorf("unexpected name %q", name)
+			}
+			if distance == 0.00 {
+				t.Errorf("distance shouldn't be empty")
+			}
+		}
+
+		// No optional parameters
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km"))
+		ok(t, err)
+		equals(t, 2, len(res))
+		for _, loc := range res {
+			item := loc.([]interface{})
+			var (
+				name, _ = redis.String(item[0], nil)
+			)
+			equals(t, 1, len(item))
+			if name != "Catania" && name != "Palermo" {
+				t.Errorf("unexpected name %q", name)
+			}
+		}
+
+		// Too small radius
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 1, "km"))
+		ok(t, err)
+		equals(t, 0, len(res))
+
+		// Wrong coords
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 80, 80, 200, "km"))
+		ok(t, err)
+		equals(t, 0, len(res))
+
+		// Wrong map key
+		res, err = redis.Values(c.Do("GEORADIUS", "Capri", 15, 37, 200, "km"))
+		ok(t, err)
+		equals(t, 0, len(res))
+
+		// Unsupported/unknown distance unit
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "mm"))
+		if err == nil {
+			t.Error("Expected error for unsupported distance unit")
+		}
+		equals(t, 0, len(res))
+
+		// Wrong parameter type
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", "abc", "def", "ghi", "m"))
+		if err == nil {
+			t.Error("Expected error for wrong parameter type")
+		}
+		equals(t, 0, len(res))
+
+		// Negative coords
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", -15, -37, 200, "m"))
+		if err == nil {
+			t.Error("Expected error for negative coords")
+		}
+		equals(t, 0, len(res))
+
+		// Negative radius
+		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, -200, "m"))
+		if err == nil {
+			t.Error("Expected error for negative radius")
+		}
+		equals(t, 0, len(res))
 	}
 }
