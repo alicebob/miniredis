5a4aea0e8294bdcc14f2713ca5e6ddc6aea92a08
Jeffrey Duroyon <jeffrey.duroyon@zenika.com>
2020-05-12 17:11:23 +0200

    add cluster keyslot and cluster nodes support

--- integration/cluster_test.go
+++ integration/cluster_test.go
@@ -7,6 +7,8 @@ import "testing"
 func TestCluster(t *testing.T) {
 	testClusterCommands(t,
 		succNoResultCheck("CLUSTER", "SLOTS"),
+		succNoResultCheck("CLUSTER", "KEYSLOT", "{test}"),
+		succNoResultCheck("CLUSTER", "NODES"),
 		failLoosely("CLUSTER"),
 	)
 }
