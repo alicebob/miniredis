e6acbfc46be9b346aa7f4b052eb72a4db33ad96c
Harmen <alicebob@lijzij.de>
2020-07-19 18:13:03 +0200

    migrate cmd_geo_test 1/2

--- cmd_geo_test.go
+++ cmd_geo_test.go
@@ -4,37 +4,49 @@ import (
 	"testing"
 
 	"github.com/gomodule/redigo/redis"
+
+	"github.com/alicebob/miniredis/v2/proto"
 )
 
 func TestGeoadd(t *testing.T) {
 	s, err := Run()
 	ok(t, err)
 	defer s.Close()
-	c, err := redis.Dial("tcp", s.Addr())
+	c, err := proto.Dial(s.Addr())
 	ok(t, err)
 	defer c.Close()
 
 	t.Run("ok", func(t *testing.T) {
-		_, err := c.Do("GEOADD", "Sicily", 13.361389, 38.115556, "Palermo")
-		ok(t, err)
-		_, err = c.Do("GEOADD", "Sicily", 15.087269, 37.502669, "Catania")
-		ok(t, err)
+		must1(t, c, "GEOADD", "Sicily", "13.361389", "38.115556", "Palermo")
+		must1(t, c, "GEOADD", "Sicily", "15.087269", "37.502669", "Catania")
 	})
 
 	t.Run("failure cases", func(t *testing.T) {
-		_, err = c.Do("GEOADD", "broken", -190.0, 10.0, "hi")
-		mustFail(t, err, "ERR invalid longitude,latitude pair -190.000000,10.000000")
-		_, err = c.Do("GEOADD", "broken", 190.0, 10.0, "hi")
-		mustFail(t, err, "ERR invalid longitude,latitude pair 190.000000,10.000000")
-		_, err := c.Do("GEOADD", "broken", 10.0, -86.0, "hi")
-		mustFail(t, err, "ERR invalid longitude,latitude pair 10.000000,-86.000000")
-		_, err = c.Do("GEOADD", "broken", 10.0, 86.0, "hi")
-		mustFail(t, err, "ERR invalid longitude,latitude pair 10.000000,86.000000")
+		mustDo(t, c,
+			"GEOADD", "broken", "-190.0", "10.0", "hi",
+			proto.Error("ERR invalid longitude,latitude pair -190.000000,10.000000"),
+		)
+		mustDo(t, c,
+			"GEOADD", "broken", "190.0", "10.0", "hi",
+			proto.Error("ERR invalid longitude,latitude pair 190.000000,10.000000"),
+		)
+		mustDo(t, c,
+			"GEOADD", "broken", "10.0", "-86.0", "hi",
+			proto.Error("ERR invalid longitude,latitude pair 10.000000,-86.000000"),
+		)
+		mustDo(t, c,
+			"GEOADD", "broken", "10.0", "86.0", "hi",
+			proto.Error("ERR invalid longitude,latitude pair 10.000000,86.000000"),
+		)
 
-		_, err = c.Do("GEOADD", "broken", "notafloat", 10.0, "hi")
-		mustFail(t, err, "ERR value is not a valid float")
-		_, err = c.Do("GEOADD", "broken", 10.0, "notafloat", "hi")
-		mustFail(t, err, "ERR value is not a valid float")
+		mustDo(t, c,
+			"GEOADD", "broken", "notafloat", "10.0", "hi",
+			proto.Error("ERR value is not a valid float"),
+		)
+		mustDo(t, c,
+			"GEOADD", "broken", "10.0", "notafloat", "hi",
+			proto.Error("ERR value is not a valid float"),
+		)
 	})
 }
 
@@ -42,33 +54,38 @@ func TestGeopos(t *testing.T) {
 	s, err := Run()
 	ok(t, err)
 	defer s.Close()
-	c, err := redis.Dial("tcp", s.Addr())
+	c, err := proto.Dial(s.Addr())
 	ok(t, err)
 	defer c.Close()
 
-	_, err = c.Do("GEOADD", "Sicily", 13.361389, 38.115556, "Palermo")
-	ok(t, err)
+	must1(t, c, "GEOADD", "Sicily", "13.361389", "38.115556", "Palermo")
 
 	t.Run("ok", func(t *testing.T) {
-		pos, err := redis.Positions(c.Do("GEOPOS", "Sicily", "Palermo"))
-		ok(t, err)
-		equals(t, 1, len(pos))
-		equals(t, [2]float64{13.361389, 38.115556}, *pos[0])
+		mustDo(t, c,
+			"GEOPOS", "Sicily", "Palermo",
+			proto.Array(
+				proto.Strings("13.361389", "38.115556"),
+			),
+		)
 	})
 
 	t.Run("no location", func(t *testing.T) {
-		pos, err := redis.Positions(c.Do("GEOPOS", "Sicily", "Corleone"))
-		ok(t, err)
-		equals(t, []*[2]float64{nil}, pos)
+		mustDo(t, c,
+			"GEOPOS", "Sicily", "Corleone",
+			proto.Array(proto.Nil),
+		)
 	})
 
 	t.Run("failure cases", func(t *testing.T) {
-		_, err = c.Do("GEOPOS")
-		mustFail(t, err, "ERR wrong number of arguments for 'geopos' command")
-		_, err = c.Do("SET", "foo", "bar")
-		ok(t, err)
-		_, err = c.Do("GEOPOS", "foo")
-		mustFail(t, err, msgWrongType)
+		mustDo(t, c,
+			"GEOPOS",
+			proto.Error(errWrongNumber("geopos")),
+		)
+		s.Set("foo", "bar")
+		mustDo(t, c,
+			"GEOPOS", "foo",
+			proto.Error(msgWrongType),
+		)
 	})
 }
 
@@ -77,159 +94,158 @@ func TestGeo(t *testing.T) {
 	s, err := Run()
 	ok(t, err)
 	defer s.Close()
-	c, err := redis.Dial("tcp", s.Addr())
+	c, err := proto.Dial(s.Addr())
 	ok(t, err)
 	defer c.Close()
 
-	_, err = c.Do("GEOADD", "Sicily", 13.361389, 38.115556, "Palermo")
-	ok(t, err)
-	_, err = c.Do("GEOADD", "Sicily", 15.087269, 37.502669, "Catania")
-	ok(t, err)
+	must1(t, c, "GEOADD", "Sicily", "13.361389", "38.115556", "Palermo")
+	must1(t, c, "GEOADD", "Sicily", "15.087269", "37.502669", "Catania")
 
 	t.Run("WITHDIST WITHCOORD", func(t *testing.T) {
-		res, err := redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHDIST", "WITHCOORD"))
-		ok(t, err)
-		equals(t, 2, len(res))
-		for _, loc := range res {
-			item := loc.([]interface{})
-			var (
-				name, _     = redis.String(item[0], nil)
-				distance, _ = redis.Float64(item[1], nil)
-				coord, _    = redis.Float64s(item[2], nil)
-			)
-			if name != "Catania" && name != "Palermo" {
-				t.Errorf("unexpected name %q", name)
-			}
-			if distance == 0.00 {
-				t.Errorf("distance shouldn't be empty")
-			}
-			equals(t, 2, len(coord))
-			if coord[0] == 0.00 || coord[1] == 0.00 {
-				t.Errorf("latitude/longitude shouldn't be empty")
-			}
-		}
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "WITHDIST", "WITHCOORD",
+			proto.Array(
+				proto.Array(
+					proto.String("Palermo"),
+					proto.String("190.4424"),
+					proto.Strings("13.361389", "38.115556"),
+				),
+				proto.Array(
+					proto.String("Catania"),
+					proto.String("56.4413"),
+					proto.Strings("15.087267", "37.502668"),
+				),
+			),
+		)
 	})
 
 	t.Run("WITHCOORD", func(t *testing.T) {
-		res, err := redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHCOORD"))
-		ok(t, err)
-		equals(t, 2, len(res))
-		for _, loc := range res {
-			item := loc.([]interface{})
-			var (
-				name, _  = redis.String(item[0], nil)
-				coord, _ = redis.Float64s(item[1], nil)
-			)
-			equals(t, 2, len(item))
-			if name != "Catania" && name != "Palermo" {
-				t.Errorf("unexpected name %q", name)
-			}
-			equals(t, 2, len(coord))
-			if coord[0] == 0.00 || coord[1] == 0.00 {
-				t.Errorf("latitude/longitude shouldn't be empty")
-			}
-		}
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "WITHCOORD",
+			proto.Array(
+				proto.Array(
+					proto.String("Palermo"),
+					proto.Strings("13.361389", "38.115556"),
+				),
+				proto.Array(
+					proto.String("Catania"),
+					proto.Strings("15.087267", "37.502668"),
+				),
+			),
+		)
 	})
 
 	t.Run("WITHDIST", func(t *testing.T) {
-		// in km
-		res, err := redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "WITHDIST"))
-		ok(t, err)
-		equals(t, 2, len(res))
-		var (
-			name1, name2 string
-			dist1, dist2 float64
+		// in KM
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "WITHDIST",
+			proto.Array(
+				proto.Strings("Palermo", "190.4424"),
+				proto.Strings("Catania", "56.4413"),
+			),
 		)
-		leftover, err := redis.Scan(res[0].([]interface{}), &name1, &dist1)
-		ok(t, err)
-		equals(t, 0, len(leftover))
-		equals(t, "Palermo", name1)
-		equals(t, 190.4424, dist1) // in km
-		_, err = redis.Scan(res[1].([]interface{}), &name2, &dist2)
-		ok(t, err)
-		equals(t, "Catania", name2)
-		equals(t, 56.4413, dist2)
 
 		// in meter
-		res, err = redis.Values(c.Do("GEORADIUS", "Sicily", 15, 37, 200000, "m", "WITHDIST"))
-		ok(t, err)
-		equals(t, 2, len(res))
-		distance, err := redis.Float64(res[0].([]interface{})[1], nil)
-		ok(t, err)
-		equals(t, 190442.4351, distance) // in meter
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200000", "m", "WITHDIST",
+			proto.Array(
+				proto.Strings("Palermo", "190442.4351"),
+				proto.Strings("Catania", "56441.2660"),
+			),
+		)
 	})
 
 	t.Run("ASC DESC", func(t *testing.T) {
-		asc, err := redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "ASC"))
-		ok(t, err)
-		equals(t, []string{"Catania", "Palermo"}, asc)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "ASC",
+			proto.Strings("Catania", "Palermo"),
+		)
 
-		desc, err := redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "DESC"))
-		ok(t, err)
-		equals(t, []string{"Palermo", "Catania"}, desc)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "DESC",
+			proto.Strings("Palermo", "Catania"),
+		)
 	})
 
 	t.Run("COUNT", func(t *testing.T) {
-		count1, err := redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "ASC", "COUNT", 1))
-		ok(t, err)
-		equals(t, []string{"Catania"}, count1)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "ASC", "COUNT", "1",
+			proto.Strings("Catania"),
+		)
 
-		count99, err := redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "ASC", "COUNT", 99))
-		ok(t, err)
-		equals(t, []string{"Catania", "Palermo"}, count99)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "ASC", "COUNT", "99",
+			proto.Strings("Catania", "Palermo"),
+		)
 
-		_, err = c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "COUNT")
-		mustFail(t, err, "ERR syntax error")
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "COUNT",
+			proto.Error("ERR syntax error"),
+		)
 
-		_, err = c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "COUNT", "notanumber")
-		mustFail(t, err, msgInvalidInt)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "COUNT", "notanumber",
+			proto.Error(msgInvalidInt),
+		)
 
-		_, err = c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km", "COUNT", -12)
-		mustFail(t, err, "ERR COUNT must be > 0")
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km", "COUNT", "-12",
+			proto.Error("ERR COUNT must be > 0"),
+		)
 	})
 
 	t.Run("no args", func(t *testing.T) {
-		res, err := redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "km"))
-		ok(t, err)
-		equals(t, 2, len(res))
-		equals(t, []string{"Palermo", "Catania"}, res)
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "km",
+			proto.Strings("Palermo", "Catania"),
+		)
 
 		// Too small radius
-		res, err = redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 1, "km"))
-		ok(t, err)
-		equals(t, 0, len(res))
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "1", "km",
+			proto.Array(),
+		)
 
 		// Wrong coords
-		res, err = redis.Strings(c.Do("GEORADIUS", "Sicily", 80, 80, 200, "km"))
-		ok(t, err)
-		equals(t, 0, len(res))
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "80", "80", "200", "km",
+			proto.Array(),
+		)
 
 		// Wrong map key
-		res, err = redis.Strings(c.Do("GEORADIUS", "Capri", 15, 37, 200, "km"))
-		ok(t, err)
-		equals(t, 0, len(res))
+		mustDo(t, c,
+			"GEORADIUS", "Capri", "15", "37", "200", "km",
+			proto.Array(),
+		)
 
 		// Unsupported/unknown distance unit
-		res, err = redis.Strings(c.Do("GEORADIUS", "Sicily", 15, 37, 200, "mm"))
-		mustFail(t, err, "ERR wrong number of arguments for 'georadius' command")
-		equals(t, 0, len(res))
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "15", "37", "200", "mm",
+			proto.Error("ERR wrong number of arguments for 'georadius' command"),
+		)
 
 		// Wrong parameter type
-		res, err = redis.Strings(c.Do("GEORADIUS", "Sicily", "abc", "def", "ghi", "m"))
-		mustFail(t, err, "ERR wrong number of arguments for 'georadius' command")
-		equals(t, 0, len(res))
+		mustDo(t, c,
+			"GEORADIUS", "Sicily", "abc", "def", "ghi", "m",
+			proto.Error("ERR wrong number of arguments for 'georadius' command"),
+		)
 	})
 
 	t.Run("GEORADIUS_RO", func(t *testing.T) {
-		asc, err := redis.Strings(c.Do("GEORADIUS_RO", "Sicily", 15, 37, 200, "km", "ASC"))
-		ok(t, err)
-		equals(t, []string{"Catania", "Palermo"}, asc)
+		mustDo(t, c,
+			"GEORADIUS_RO", "Sicily", "15", "37", "200", "km", "ASC",
+			proto.Strings("Catania", "Palermo"),
+		)
 
-		_, err = c.Do("GEORADIUS_RO", "Sicily", 15, 37, 200, "km", "STORE", "foo")
-		mustFail(t, err, "ERR syntax error")
+		mustDo(t, c,
+			"GEORADIUS_RO", "Sicily", "15", "37", "200", "km", "STORE", "foo",
+			proto.Error("ERR syntax error"),
+		)
 
-		_, err = c.Do("GEORADIUS_RO", "Sicily", 15, 37, 200, "km", "STOREDIST", "foo")
-		mustFail(t, err, "ERR syntax error")
+		mustDo(t, c,
+			"GEORADIUS_RO", "Sicily", "15", "37", "200", "km", "STOREDIST", "foo",
+			proto.Error("ERR syntax error"),
+		)
 	})
 }
 
