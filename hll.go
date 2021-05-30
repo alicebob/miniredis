b47de9ad4b4badcfd6f64ceea05fcae85a5575a2
Konstantin Ilbaktin <konstantin.ilbaktin@fingerprintjs.com>
2021-05-31 01:18:45 +0300

    add more comments to hll-related methods

--- hll.go
+++ hll.go
@@ -20,14 +20,17 @@ func (h *hll) Add(item []byte) bool {
 	return h.inner.InsertHash(murmur3.Sum64(item))
 }
 
+// Count returns the estimation of a set cardinality.
 func (h *hll) Count() int {
 	return int(h.inner.Estimate())
 }
 
+// Merge merges the other hll into original one (not making a copy but doing this in place).
 func (h *hll) Merge(other *hll) {
 	_ = h.inner.Merge(other.inner)
 }
 
+// Bytes returns raw-bytes representation of hll data structure.
 func (h *hll) Bytes() []byte {
 	dataBytes, _ := h.inner.MarshalBinary()
 	return dataBytes
