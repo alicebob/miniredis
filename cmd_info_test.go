8c036ad6b9dcceb0f01f8c9d85b045b0ee5407f0
Kirill Belov <kirill.a.belov@gmail.com>
2022-06-17 13:58:34 +0200

    INFO_partially_support: Fix tests

--- cmd_info_test.go
+++ cmd_info_test.go
@@ -24,21 +24,21 @@ func TestMiniredis_cmdInfo(t *testing.T) {
 	t.Run("No section name in args", func(t *testing.T) {
 		mustDo(t, c,
 			"INFO",
-			proto.Strings("# Clients\nconnected_clients:1\r\n"),
+			proto.String("# Clients\nconnected_clients:1\r\n"),
 		)
 	})
 
 	t.Run("Success", func(t *testing.T) {
 		mustDo(t, c,
 			"INFO", "clients",
-			proto.Strings("# Clients\nconnected_clients:1\r\n"),
+			proto.String("# Clients\nconnected_clients:1\r\n"),
 		)
 
 		c2, err := proto.Dial(s.Addr())
 		ok(t, err)
 		mustDo(t, c2,
 			"INFO", "clients",
-			proto.Strings("# Clients\nconnected_clients:2\r\n"),
+			proto.String("# Clients\nconnected_clients:2\r\n"),
 		)
 		c2.Close()
 
@@ -47,7 +47,7 @@ func TestMiniredis_cmdInfo(t *testing.T) {
 		defer c3.Close()
 		mustDo(t, c3,
 			"INFO", "clients",
-			proto.Strings("# Clients\nconnected_clients:2\r\n"),
+			proto.String("# Clients\nconnected_clients:2\r\n"),
 		)
 	})
 }
