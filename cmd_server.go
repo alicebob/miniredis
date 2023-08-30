b3df4a31c735471992c7c4e955fd92b04eff38cd
David Roman <romandavid83@gmail.com>
2023-08-30 00:35:30 -0400

    add: memory usage
    notes
    
    based on master key
    
    After re-reading the doc and re-trying on my side on different types, indeed we need to do it across everything
    
    I took the suggestion of @alicebob to put the dependency in a folder and credit, I don't know for the polishing required
    
    Added small test

--- cmd_server.go
+++ cmd_server.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/alicebob/miniredis/v2/server"
+	"github.com/alicebob/miniredis/v2/size"
 )
 
 func commandsServer(m *Miniredis) {
@@ -16,6 +17,80 @@ func commandsServer(m *Miniredis) {
 	m.srv.Register("FLUSHDB", m.cmdFlushdb)
 	m.srv.Register("INFO", m.cmdInfo)
 	m.srv.Register("TIME", m.cmdTime)
+	m.srv.Register("MEMORY", m.cmdMemory)
+}
+
+// MEMORY
+func (m *Miniredis) cmdMemory(c *server.Peer, cmd string, args []string) {
+	if len(args) == 0 {
+		setDirty(c)
+		c.WriteError(errWrongNumber(cmd))
+		return
+	}
+	if !m.handleAuth(c) {
+		return
+	}
+	if m.checkPubsub(c, cmd) {
+		return
+	}
+
+	withTx(m, c, func(c *server.Peer, ctx *connCtx) {
+		db := m.db(ctx.selectedDB)
+
+		switch args[0] {
+
+		case "USAGE":
+			var value interface{}
+			var ok bool
+
+			switch db.keys[args[1]] {
+			case "string":
+				if value, ok = db.stringKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+			case "set":
+				if value, ok = db.setKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+			case "hash":
+				if value, ok = db.hashKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+			case "list":
+				if value, ok = db.listKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+
+			case "hll":
+				if value, ok = db.hllKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+
+			case "zset":
+				if value, ok = db.sortedsetKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+
+			case "stream":
+				if value, ok = db.streamKeys[args[1]]; ok {
+					c.WriteInt(size.Of(value))
+					return
+				}
+			}
+
+			c.WriteError(ErrKeyNotFound.Error())
+			break
+		default:
+			c.WriteError(errWrongNumber(cmd))
+		}
+
+	})
 }
 
 // DBSIZE
