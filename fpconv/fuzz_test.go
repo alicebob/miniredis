85eaaadee904d2ab9c37afafbf23dd783ec8a666
Harmen <alicebob@lijzij.de>
2023-10-04 12:57:50 +0200

    use grisu2 for formatting (#347)
    port grisu2 formatter from redis src

---
+++ fpconv/fuzz_test.go
@@ -0,0 +1,44 @@
+//go:build go1.16
+// +build go1.16
+
+package fpconv
+
+import (
+	"math"
+	"strconv"
+	"testing"
+)
+
+func FuzzDtoa(f *testing.F) {
+	f.Add(-3.1415)
+	f.Add(0.0)
+	f.Add(2.5339988685347402e-65)
+	f.Add(3.1415)
+	f.Add(math.Inf(1))
+	f.Add(math.Inf(-1))
+	f.Add(math.NaN())
+
+	f.Fuzz(func(t *testing.T, orig float64) {
+		s := Dtoa(orig)
+		if s == "" {
+			t.Errorf("empty result")
+		}
+		n, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			t.Errorf("parse failed: %s", err)
+		}
+		if math.IsNaN(orig) {
+			if !math.IsNaN(n) {
+				t.Error("not NaN")
+			}
+			return
+		}
+
+		if math.IsNaN(n) {
+			t.Error("got NaN")
+		}
+		if n != orig {
+			t.Errorf("changed %f -> %f", n, orig)
+		}
+	})
+}
