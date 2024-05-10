1e417579d16d15d6d1f2de32c09cb653413103cb
Joakim Hamren <joakim.hamren@gmail.com>
2024-05-10 22:20:26 +0200

    few more test cases

--- cmd_stream_test.go
+++ cmd_stream_test.go
@@ -189,7 +189,19 @@ func TestStreamAdd(t *testing.T) {
 
 	t.Run("XADD NOMKSTREAM", func(t *testing.T) {
 		mustDo(t, c,
+			"XADD", "reallynosuchkey", "NOMKSTREAM", "*", "one", "1",
+			proto.Nil,
+		)
+		mustDo(t, c,
+			"XADD", "reallynosuchkey", "NOMKSTREAM", "MINID", "1672545848004-0", "*", "one", "1",
+			proto.Nil,
+		)
+		mustDo(t, c,
 			"XADD", "reallynosuchkey", "NOMKSTREAM", "MAXLEN", "~", "10", "*", "one", "1",
+			proto.Nil,
+		)
+		mustDo(t, c,
+			"XADD", "reallynosuchkey", "NOMKSTREAM", "MAXLEN", "~", "10", "MINID", "1672545848004-0", "*", "one", "1",
 			proto.Nil,
 		)
 	})
